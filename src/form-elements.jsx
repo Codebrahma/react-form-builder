0371f06d915f3abe16e13e5a94bc0a4ddf8fd45c
Sathish M <design.sathish@gmail.com>
2025-01-21 02:29:22 +0530

    Add duplication to linebreak element

--- src/form-elements.jsx
+++ src/form-elements.jsx
@@ -185,6 +185,7 @@ class LineBreak extends React.Component {
               onEdit={this.props.onEdit}
               static={this.props.data.static}
               required={this.props.data.required}
+              onDuplicate={this.props._onDuplicate}
             />
           </div>
         )}
