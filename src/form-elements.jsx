0a778b74f36f2f43ff5a0b70aef2d381cd9601b4
Sathish M <design.sathish@gmail.com>
2025-01-21 02:40:07 +0530

    Add duplication to tags element

--- src/form-elements.jsx
+++ src/form-elements.jsx
@@ -836,6 +836,7 @@ class Tags extends React.Component {
               onEdit={this.props.onEdit}
               static={this.props.data.static}
               required={this.props.data.required}
+              onDuplicate={this.props._onDuplicate}
             />
           </div>
         )}
