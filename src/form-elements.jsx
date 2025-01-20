ab4df82dd196218c751d6b56a937b64dc3f3f47e
Sathish M <design.sathish@gmail.com>
2025-01-21 02:28:44 +0530

    Add duplication to number input element

--- src/form-elements.jsx
+++ src/form-elements.jsx
@@ -300,6 +300,7 @@ class NumberInput extends React.Component {
               onEdit={this.props.onEdit}
               static={this.props.data.static}
               required={this.props.data.required}
+              onDuplicate={this.props._onDuplicate}
             />
           </div>
         )}
