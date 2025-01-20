90b90e122b7929ab7cb9cd3d6bf5cb956d22cdd7
Sathish M <design.sathish@gmail.com>
2025-01-21 02:54:38 +0530

    Add duplication to rating element

--- src/form-elements.jsx
+++ src/form-elements.jsx
@@ -1130,6 +1130,7 @@ class Rating extends React.Component {
               onEdit={this.props.onEdit}
               static={this.props.data.static}
               required={this.props.data.required}
+              onDuplicate={this.props._onDuplicate}
             />
           </div>
         )}
