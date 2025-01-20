d95ca2be119a6f83bfd4fb192797fc3975ccdad9
Sathish M <design.sathish@gmail.com>
2025-01-21 02:47:09 +0530

    Add duplication to checkbox element

--- src/form-elements.jsx
+++ src/form-elements.jsx
@@ -893,6 +893,7 @@ class Checkboxes extends React.Component {
               onEdit={this.props.onEdit}
               static={this.props.data.static}
               required={this.props.data.required}
+              onDuplicate={this.props._onDuplicate}
             />
           </div>
         )}
